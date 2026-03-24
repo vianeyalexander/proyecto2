5f75bb9584f2de3d29382414ed89f236bf1270b6
User <vianeyalex252@gmail.com>
2026-03-24 18:38:58 +0100

    inicio proyecto

---
+++ Telefono.java
@@ -0,0 +1,11 @@
+class Telefono {
+    public string forma;
+    private integer bateria;
+    protected string camara;
+    
+    public void vender(){
+    }
+    private void reproducirVideo(){
+        
+    }
+}
