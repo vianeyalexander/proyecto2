0ebbaa271ce945d07df19a2527884d2dcc85738a
stephany montufar <nicolemontufar07@gmail.com>
2026-03-24 11:31:05 -0600

    inicio proyecto

---
+++ Telefono.java
@@ -0,0 +1,10 @@
+class Telefono {
+    public String forma;
+    private Integer bateria;
+    protected String camara;
+
+    public void vender(){
+    }
+    private void reproducirVideo(){
+    }
+}
