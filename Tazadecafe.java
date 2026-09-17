a431572534602abdbdbe6a48bcf81567502a9574
User <vianeyalex252@gmail.com>
2026-09-17 01:08:29 -0300

    CreadorObjeto.java

---
+++ Tazadecafe.java
@@ -0,0 +1,20 @@
+public class Tazadecafe {
+    public String color;
+    public String marca;
+    public Integer capacidad;
+    public Double precio;
+     
+    public Tazadecafe(String nombredelaclase){
+
+     //   nombre=nombredelaclase;
+        capacidad=300;
+
+        System.out.println(color);
+        System.out.println(nombredelaclase);
+    }
+
+    public void tomar(){
+
+
+    }
+}
