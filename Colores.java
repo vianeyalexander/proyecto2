ac99087c410e0d7c7b140c1cae2a483752701e57
User <vianeyalex252@gmail.com>
2026-09-16 23:41:03 -0300

    ¨colores¨

---
+++ Colores.java
@@ -0,0 +1,7 @@
+public class Colores {
+    public static final String VERDE="\u001B[32m";
+    public static final String ROJO="\u001B[31m";
+    public static final String AMARILLO="\u001B[33m";
+    public static final String RESET="\u001B[0m";
+    
+}
